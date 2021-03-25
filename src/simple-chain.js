65d3e9f87ad2042e3b204f688b8e0350d1581b0d
MikhailPilipenko <pmgomel@mail.ru>
2021-03-26 01:02:43 +0300

    feat: add object chainMaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) {
+      this.chain.push(' ');
+    } else {
+      this.chain.push(String(value));
+    }
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position <= 0 || position > (this.getLength() - 1) || typeof position !== 'number') {
+      this.chain = [];
+      throw new Error("Invalid position");
+    } else {
+      this.chain.splice(position - 1, 1);
+    }
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = `( ${this.chain.join(' )~~( ')} )`;
+    this.chain = [];
+    return result;
   }
 };
 
